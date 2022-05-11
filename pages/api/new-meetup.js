69416692204ca2b16d7909142e6e452db441ec42
Samruddhi87 <samarekar87@gmail.com>
2022-05-11 19:30:50 +0530

    ready to deploy

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,21 @@
+import { MongoClient } from "mongodb";
+// /api/new-meetup
+// POST/api/new-meetup
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+    // const{title,image,address,description}=data;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://Samruddhi:Samarekar87@cluster0.0cogj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+    const meetupsCollection = db.collection("meetups");
+    const result =  await meetupsCollection.insertOne(data);
+    console.log(result);
+    client.close();
+    res.status(201).json({ message: "meetups inserted" });
+  }
+}
+export default handler;
